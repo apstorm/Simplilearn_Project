262783e5921986301fce71ec6a8da73bb82baa2c
Atharv <aps15sep@gmail.com>
2021-08-14 20:42:24 +0530

    Phase-1 Project Files

---
+++ SourceCode/LokedMe_Phase1_Project/src/com/lokedme/FileManaging.java
@@ -0,0 +1,110 @@
+package com.lokedme;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileManaging 
+{
+/**
+ * this method will return the file names from folder 
+ * @param folderPath
+ * @return list<String>
+ */
+	public static List<String> getAllFiles(String folderPath)
+	{
+		//file object creation
+		File fl=new File(folderPath);
+		
+		//getting all files inside array of file
+		File[] listOfFiles=fl.listFiles();
+		
+		//declare list to store file names
+		List<String> fileNames=new ArrayList<String>();
+		
+		for(File f:listOfFiles)
+		{
+			fileNames.add(f.getName());
+		}
+		
+		//return list
+		return fileNames;
+		
+	}
+	/**
+	 * this method create or append content into the specified file
+	 * @param folderPath
+	 * @param fileName
+	 * @param content
+	 * @return
+	 */
+	public static boolean createFile(String folderPath,String fileName, List<String> content)
+	{
+		try
+		{
+			File fl=new File(folderPath, fileName);
+			FileWriter fw=new FileWriter(fl);
+			for(String s:content)
+			{
+				fw.write(s+"\n");
+			}
+			fw.close();
+			return true;
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+		
+	}
+	
+	/**
+	 * this method will delete the file name specified
+	 * @param folderPath
+	 * @param fileName
+	 * @return
+	 */
+	public static boolean deleteFile(String folderPath,String fileName)
+	{
+		//adding folder path with filename and create file object
+		File f=new File(folderPath+"\\"+fileName);
+		try
+		{
+			if(f.delete())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		catch (Exception e)
+		{
+			return false;
+		}
+	}
+	
+	public static boolean searchFile(String folderPath,String fileName)
+	{
+		//adding folder path with filename and create file object
+		File f=new File(folderPath+"\\"+fileName);
+		try
+		{
+			if(f.exists())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		catch (Exception e)
+		{
+			return false;
+		} 
+
+	}
+}
---
+++ SourceCode/LokedMe_Phase1_Project/src/com/lokedme/LokedMeProj.java
@@ -0,0 +1,154 @@
+package com.lokedme;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class LokedMeProj {
+
+	static final String folderPath="F:\\EGDownloads\\Simplilearn stuffs\\live class vedeos\\P-1\\Project(Phase-1)\\(LockedMe.com)-files";
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("************************************************************");
+		System.out.println("\t\tCompany Lockers Pvt. Ltd.");
+		System.out.println("************************************************************");
+		System.out.println("**** 1. Display all files **********************************");
+		System.out.println("**** 2. Add new file ***************************************");
+		System.out.println("**** 3. Delete a file **************************************");
+		System.out.println("**** 4. Search a file **************************************");
+		System.out.println("**** Enter your choice: ************************************");
+		System.out.println("************************************************************");
+		
+	}
+	
+	/**
+	 * Description: this method is used for retrieving all 
+	 * 				files in ascending order
+	 * 
+	 * Author: 		Atharv Pratap Singh
+	 * 
+	 * Date:		13/08/2021
+	 */
+	public static void getAllFiles()
+	{
+		List<String> fileNames=FileManaging.getAllFiles(folderPath);
+		
+		// Shows all files in folder but acending remains
+		for(String s:fileNames)
+		{
+			System.out.println(s);
+		}
+	}
+	
+	/**
+	 * Description: this method is used to add a new file
+	 * 				along with some lines of content inside
+	 * 				it
+	 * 
+	 * Author: 		Atharv Pratap Singh
+	 * 
+	 * Date:		13/08/2021
+	 */
+	public static void createFile()
+	{
+		// Add file and content code
+		
+		//variable declaration
+		Scanner obj=new Scanner(System.in);
+		String fileName;
+		int linesCount;
+		List<String> content=new ArrayList<String>();
+		
+		//read file name from user
+		System.out.println("enter file name: ");
+		fileName=obj.nextLine();
+		
+		//read number of lines
+		System.out.println("enter how many lines in file ?");
+		linesCount=Integer.parseInt(obj.nextLine());
+		
+		//read lines from user input
+		for(int i=1;i<=linesCount;i++)
+		{
+			System.out.println("enter line "+i+":");
+			content.add(obj.nextLine());
+		}
+		
+		
+		//saving the content into the file
+		boolean isSaved=FileManaging.createFile(folderPath, fileName, content);
+		
+		if(isSaved)
+		{
+			System.out.println("file data saved successfully");
+		}
+		else
+		{
+			System.out.println("some error is there. please contact system adminisstrator");
+		}
+		
+		//closing scanner object
+		obj.close();
+	}
+	
+	/**
+	 * Description: this method is used for deleting 
+	 * 				the specified file in the folder
+	 * 
+	 * Author: 		Atharv Pratap Singh
+	 * 
+	 * Date:		14/08/2021
+	 */
+	public static void deleteFile()
+	{
+		// Deleting the specified file
+		String fileName;
+		Scanner obj=new Scanner(System.in);
+		System.out.println("enter file name to be deleted: ");
+		fileName=obj.nextLine();
+		
+		boolean isDeleted=FileManaging.deleteFile(folderPath, fileName);
+		//deleting the file
+		if(isDeleted)
+		{
+			System.out.println("file deleted successfully");
+		}
+		else
+		{
+			System.out.println("file not there or may be some other issue...");
+		}
+		
+	}
+	
+	/**
+	 * Description: this method is used to search
+	 * 				the specified file to know 
+	 * 				whether is it present or not
+	 * 				in the folder
+	 * 
+	 * Author: 		Atharv Pratap Singh
+	 * 
+	 * Date:		14/08/2021
+	 */
+	public static void searchFile()
+	{
+			// code for Searching a specified file
+			String fileName;
+			Scanner obj=new Scanner(System.in);
+			System.out.println("enter the file to be searched: ");
+			fileName=obj.nextLine();
+			
+			boolean isFound=FileManaging.searchFile(folderPath, fileName);
+			
+			if(isFound)
+			{
+				System.out.println("file is found, present");
+			}
+			else
+			{
+				System.out.println("file not found, absent");
+			}
+	}
+
+}
